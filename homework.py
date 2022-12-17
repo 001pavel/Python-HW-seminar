76c5619142637d9cd07891dc1e38e81160172b9a
PavelKrak <tropicpost@mail.ru>
2022-12-17 15:46:01 +0300

    Домашнее задание по 4 семинару

--- homework.py
+++ homework.py
@@ -1,140 +1,131 @@
 import math
 import random
 
-# Домашнее задание по третьему семинару
+# Домашнее задание по четвертому семинару
+# 1. ДЗ	Вычислить число c заданной точностью d
 
-# 1. ДЗ	Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
-# Пример:
-# o	[2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
-# import random
-# sp1 = [2, 3, 5, 9, 3]
-# sp3 = list(range(5))
+# from math import pi
+# d = input('Enter number: ').split('.')
+# num = str(pi).split('.')
+# print(num[0] + '.' + num[1][:len(d[1])])
 
-
-# def random_sp(n):
-#     sp = []
-#     for i in range(n):
-#         sp.append(random.randrange(1, n))
-#     return sp
+# 2. ДЗ	Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
 
-# def sum_odd_position(sp):
-#     count = 0
-#     for i in range(len(sp)):
-#         if i % 2 != 0:
-#             count += sp[i]
-#     print(count)
+# def find_lst_simple(n):
+#     min_simple = 2  # минимальное натуральное число
+#     lst = []
+#     while n >= min_simple:
+#         while n % min_simple == 0:
+#             lst.append(min_simple)
+#             n /= min_simple
+#         min_simple += 1
+#     return lst
 
 
-# sp2 = random_sp(5)
-# print(sp2)
-# sum_odd_position(sp2)
+# n = int(input('Введите натуральное число: '))
+# lst = find_lst_simple(n)
+# print(f'{lst} - список простых множителей числа {n}')
 
-# 2.	Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-# Пример:
-# o	[2, 3, 4, 5, 6] => [12, 15, 16];
-# o	[2, 3, 5, 6] => [12, 15]
-# sp1 = [2, 3, 4, 5, 6]
-# sp3 = [2, 3, 5, 6]
-
+# 3. ДЗ	Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# [1, 2, 2, 3, 4]  -> [1, 3, 4]
 
 # def random_sp(n):
 #     sp = []
 #     for i in range(n):
-#         sp.append(random.randrange(1, n + 1))
+#         sp.append(random.randrange(n))
 #     return sp
 
 
-# def mult_sp(sp):
-#     mult = 1
-#     for i in range(len(sp)//2):
-#         mult = sp[i] * sp[-1-i]
-#         print(mult, end=' ')
-#     if len(sp) % 2 != 0:
-#         print(sp[(len(sp) // 2)]**2, end=' ')
+# sp = random_sp(6)
+# print(f'{sp} - заданная последовательность чисел')
+# sp_new = []
+# for i in sp:
+#     if sp.count(i) == 1:
+#         sp_new.append(i)
 
+# print(f'{sp_new} - список неповторяющихся элементов исходной последовательности')
 
-# sp2 = random_sp(5)
-# print(sp2)
-# mult_sp(sp2)
+# 4. ДЗ	Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 
-# 3.	Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
-# Пример:
-# o	[1.1, 1.2, 3.1, 5, 10.01] => 0.19
-# sp1 = [1.1, 1.2, 3.1, 5, 10.01]
-# sp3 = [0.03, 1.17, 9.62, 7.2, 4.86]
+# data = open('002.txt', 'w')
+# k = 3
 
 
 # def random_sp(n):
 #     sp = []
-#     for i in range(n):
-#         sp.append(round(random.random()*10, 2))
+#     for i in range(n + 1):
+#         sp.append(random.randrange(100))
 #     return sp
 
 
-# def find_dif(sp):
-#     max_num = sp[0] % 1
-#     min_num = sp[0] % 1
-#     for i in range(len(sp)):
-#         after_dot = sp[i] % 1
-#         # print(f"after dot: {after_dot}")
-#         if max_num < after_dot:
-#             max_num = after_dot
-#             # print(f"max: {max_num}")
-#         if min_num > after_dot:
-#             min_num = after_dot
-#             # print(f"min: {min_num}")
-#     print(round(max_num - min_num, 2))
+# sp = random_sp(k)
+# # print(sp)
+# st = ''
+# for i in range(len(sp)):
+#     if sp[0] == 0:
+#         break
+#     elif k > 1 and sp[i] > 1:
+#         st += ' + ' + str(sp[i]) + 'x^' + str(k)
+#         k -= 1
+#     elif k > 1 and sp[i] == 1:
+#         st += ' + x^' + str(k)
+#         k -= 1
+#     elif k > 1 and sp[i] == 0:
+#         st += ''
+#         k -= 1
+#     elif k == 1 and sp[i] > 1:
+#         st += ' + ' + str(sp[i]) + 'x'
+#         k -= 1
+#     elif k == 1 and sp[i] == 1:
+#         st += ' + x'
+#         k -= 1
+#     elif k == 1 and sp[i] == 0:
+#         k -= 1
+#     elif k == 0 and sp[i] != 0:
+#         st += ' + ' + str(sp[i])
 
+# if st:
+#     print(st[3:] + ' = 0')
+#     data.writelines(st[3:] + ' = 0')
 
-# sp2 = random_sp(5)
+# data.close()
+# 5. ДЗ	Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+
+path = '001.txt'
+data = open(path, 'r')
+for line in data:
+    pn1 = line
+data.close()
+
+path = '002.txt'
+data = open(path, 'r')
+for line in data:
+    pn2 = line
+data.close()
+
+# f = '3x^2 + 5x + -9 = 0'
+pn1 = pn1[: -4]
+pn1 = pn1.split(' + ')
+# print(pn1)
+sp1 = []
+for i in pn1:
+    sp1.append(int(i.split('x')[0]))
+# print(sp1)
+a1, b1, c1, d1 = sp1
+
+pn2 = pn2[: -4]
+pn2 = pn2.split(' + ')
+# print(pn2)
+sp2 = []
+for i in pn2:
+    sp2.append(int(i.split('x')[0]))
 # print(sp2)
-# find_dif(sp2)
+a2, b2, c2, d2 = sp2
+a3, b3, c3, d3 = a1 + a2, b1 + b2, c1 + c2, d1 + d2
 
-# 4.	Напишите программу, которая будет преобразовывать десятичное число в двоичное (встроенными методами пользоваться нельзя).
-# Пример:
-# o	45 -> 101101
-# o	3 -> 11
-# o	2 -> 10
+print(f'{a3}x^3 + {b3}x^2 + {c3}x + {d3} = 0')
 
-# def conv_to_binary(n):
-#     sp = []
-#     while n > 0:
-#         sp.append(n % 2)
-#         n //= 2
-#     for i in range(len(sp)):
-#         print(sp[-1-i], end=' ')
-
-
-# conv_to_binary(int(input('Enter number: ')))
-
-# 5.	Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
-# Пример:
-# o	для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
-
-def fib(n):
-    if n in [1, 2]:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
-
-
-def nfib(n):
-    if n == 1:
-        return 1
-    elif n == 0:
-        return 0
-    else:
-        return nfib(n+2) - nfib(n+1)
-
-
-k = 8
-sp = []
-
-for i in range(-k, k + 1):
-    if i < 2:
-        sp.append(nfib(i))
-    else:
-        sp.append(fib(i))
-
-print(sp)
+data = open('003.txt', 'w')
+# запись суммы многочленов в новый файл 003.txt
+data.writelines(f'{a3}x^3 + {b3}x^2 + {c3}x + {d3} = 0')
